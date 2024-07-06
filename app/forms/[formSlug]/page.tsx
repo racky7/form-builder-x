a664e8fa577aa6694ae70f31afb308e6fe91f9ca
racky7 <raj@tealfeed.com>
2024-07-07 02:56:08 +0530

    feat: rendered the form using react-hook-form

--- app/forms/[formSlug]/page.tsx
+++ app/forms/[formSlug]/page.tsx
@@ -2,31 +2,64 @@
 
 import { trpc } from "@/lib/trpc/client";
 import { useParams } from "next/navigation";
-import { match } from "ts-pattern";
+import { P, match } from "ts-pattern";
 import { useForm } from "react-hook-form";
 import {
   Form,
   FormControl,
   FormField,
   FormItem,
+  FormLabel,
   FormMessage,
 } from "@/components/ui/form";
 import {
   FormField as FormFieldType,
   generateValidationSchema,
 } from "@/lib/form";
-import { useEffect, useRef } from "react";
-import { Form as FormType } from "@prisma/client";
+import { useMemo } from "react";
+import invariant from "tiny-invariant";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { cn } from "@/lib/utils";
+import { Textarea } from "@/components/ui/textarea";
 
 export default function Page() {
   const params = useParams<{ formSlug: string }>();
   const getFormQuery = trpc.builder.getForm.useQuery({ slug: params.formSlug });
-  const fieldRefs = useRef<{ [key: string]: string }>({});
 
-  useEffect(() => {
-    if (getFormQuery.data) {
-    }
-  }, [getFormQuery.data]);
+  const form = useForm<any>({
+    mode: "all",
+    defaultValues: useMemo(() => {
+      let defaultValuesObject: Record<string, string> = {};
+
+      if (getFormQuery.data) {
+        const fieldsOrder = getFormQuery.data.fieldsOrder as string[];
+        fieldsOrder.forEach((fieldId) => {
+          defaultValuesObject[fieldId] = "";
+        });
+      }
+      return defaultValuesObject;
+    }, [getFormQuery.data]),
+    resolver: zodResolver(
+      useMemo(() => {
+        let validationSchema = z.object({});
+        if (getFormQuery.data) {
+          const fieldsSchema = getFormQuery.data.fieldsSchema as Record<
+            string,
+            FormFieldType
+          >;
+          validationSchema = generateValidationSchema(fieldsSchema);
+        }
+        return validationSchema;
+      }, [getFormQuery.data])
+    ),
+  });
+
+  const onSubmit = (values: any) => {
+    console.log(values);
+  };
 
   return match(getFormQuery)
     .returnType<React.ReactNode>()
@@ -45,25 +78,106 @@ export default function Page() {
 
       return (
         <div className="bg-primary/10 w-full min-h-screen flex justify-center">
-          <form className="flex flex-col space-y-3 max-w-[640px] w-full h-full pt-3">
-            {fieldsOrder.map((fieldId) => {
-              const { name } = fieldsSchema[fieldId];
-              return (
-                <div
-                  key={fieldId}
-                  className="w-full bg-white rounded-lg border pt-1 border-gray-300 h-28"
-                >
-                  <div className="w-full h-4 flex"></div>
-                  <div className="space-y-2 px-6 pb-6">
-                    <div
-                      className="text-sm"
-                      dangerouslySetInnerHTML={{ __html: name }}
-                    />
-                  </div>
-                </div>
-              );
-            })}
-          </form>
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="flex flex-col space-y-3 max-w-[640px] w-full h-full pt-3"
+            >
+              {fieldsOrder.map((fieldId) => {
+                const { name, field: formField } = fieldsSchema[fieldId];
+                return match(formField.type)
+                  .returnType<React.ReactNode>()
+                  .with("input", () => {
+                    invariant(formField.type === "input");
+                    return match(formField.inputType)
+                      .returnType<React.ReactNode>()
+                      .with("short-input", () => {
+                        return (
+                          <FormField
+                            control={form.control}
+                            key={fieldId}
+                            name={fieldId}
+                            render={({ field }) => (
+                              <div
+                                className={cn(
+                                  "w-full bg-white rounded-lg border pt-1 border-gray-300",
+                                  form.formState.errors[fieldId] !== undefined
+                                    ? "border-red-500"
+                                    : null
+                                )}
+                              >
+                                <div className="w-full h-4 flex"></div>
+                                <FormItem className="space-y-3 px-6 pb-6">
+                                  <FormLabel asChild>
+                                    <div
+                                      className="text-sm"
+                                      dangerouslySetInnerHTML={{
+                                        __html: name,
+                                      }}
+                                    />
+                                  </FormLabel>
+                                  <FormControl>
+                                    <Input
+                                      {...field}
+                                      placeholder={formField.placeholder}
+                                    />
+                                  </FormControl>
+                                  <FormMessage />
+                                </FormItem>
+                              </div>
+                            )}
+                          />
+                        );
+                      })
+                      .with("long-input", () => {
+                        return (
+                          <FormField
+                            control={form.control}
+                            key={fieldId}
+                            name={fieldId}
+                            render={({ field }) => (
+                              <div
+                                className={cn(
+                                  "w-full bg-white rounded-lg border pt-1 border-gray-300",
+                                  form.formState.errors[fieldId] !== undefined
+                                    ? "border-red-500"
+                                    : null
+                                )}
+                              >
+                                <div className="w-full h-4 flex"></div>
+                                <FormItem className="space-y-3 px-6 pb-6">
+                                  <FormLabel asChild>
+                                    <div
+                                      className="text-sm"
+                                      dangerouslySetInnerHTML={{
+                                        __html: name,
+                                      }}
+                                    />
+                                  </FormLabel>
+                                  <FormControl>
+                                    <Textarea
+                                      {...field}
+                                      placeholder={formField.placeholder}
+                                    />
+                                  </FormControl>
+                                  <FormMessage />
+                                </FormItem>
+                              </div>
+                            )}
+                          />
+                        );
+                      })
+                      .with(P._, () => null)
+                      .exhaustive();
+                  })
+                  .with(P._, () => null)
+                  .exhaustive();
+              })}
+              <div>
+                <Button type="submit">Submit</Button>
+              </div>
+            </form>
+          </Form>
         </div>
       );
     })
