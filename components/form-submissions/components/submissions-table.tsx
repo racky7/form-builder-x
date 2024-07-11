ddfc017b1f2e6f9fe3434d5b54bc05a4fa74d0d8
racky7 <raj@tealfeed.com>
2024-07-12 00:22:57 +0530

    feat: rendered the submission table

--- components/form-submissions/components/submissions-table.tsx
+++ components/form-submissions/components/submissions-table.tsx
@@ -1,4 +1,21 @@
-import { ColumnDef } from "@tanstack/react-table";
+"use client";
+
+import {
+  ColumnDef,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { useFormBuilderContext } from "@/context";
 
 type SingleSubmission = {
   [fieldId: string]: string | null;
@@ -11,12 +28,66 @@ type SubmissionsTableProps = {
 export default function SubmissionsTable({
   submissionsData,
 }: SubmissionsTableProps) {
-  const columns: ColumnDef<SingleSubmission>[] = submissionsData.map((key) => {
+  const { fieldsOrder, fieldsSchema } = useFormBuilderContext();
+  const columns: ColumnDef<SingleSubmission>[] = fieldsOrder.map((fieldId) => {
+    const element = document.createElement("div");
+    element.innerHTML = fieldsSchema[fieldId].name;
     return {
-      header: "XYZ FieldName",
-      accessorKey: key,
+      header: element.innerText,
+      accessorKey: fieldId,
     };
   });
 
-  return <div></div>;
+  const table = useReactTable({
+    data: submissionsData,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
+
+  return (
+    <div className="rounded-md border">
+      <Table>
+        <TableHeader>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <TableRow key={headerGroup.id}>
+              {headerGroup.headers.map((header) => {
+                return (
+                  <TableHead key={header.id}>
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                  </TableHead>
+                );
+              })}
+            </TableRow>
+          ))}
+        </TableHeader>
+        <TableBody>
+          {table.getRowModel().rows?.length ? (
+            table.getRowModel().rows.map((row) => (
+              <TableRow
+                key={row.id}
+                data-state={row.getIsSelected() && "selected"}
+              >
+                {row.getVisibleCells().map((cell) => (
+                  <TableCell key={cell.id}>
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </TableCell>
+                ))}
+              </TableRow>
+            ))
+          ) : (
+            <TableRow>
+              <TableCell colSpan={columns.length} className="h-24 text-center">
+                No submissions.
+              </TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+    </div>
+  );
 }
